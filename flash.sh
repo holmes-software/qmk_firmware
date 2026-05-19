d75981d32e28bab39fce19e66483fa36843d2d7e
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2026-05-18 19:21:03 -0700

    Add comments about how to run

--- flash.sh
+++ flash.sh
@@ -1,3 +1,21 @@
+# This script flashes the compiled firmware to a Ploopy device.
+#
+# Perform the following before running this script:
+#   1. Configure. Duh. Configs are `keyboards/ploopyco/madromys/keymaps/holmes_software/keymap.c` and
+#      `keyboards/ploopyco/madromys/config.h:34`.
+#   2. Compile firmware. Run `qmk compile`. Defaults are already set.
+#   3. Enter bootloader mode. To put the device in bootloader mode, hold left click and plug the mouse in. It should
+#      then appear in `lsblk` results. You will need those results when running this script.
+#
+# Then run the script to flash!
+#
+# Several checks are in place to be sure you don't do something dumb:
+#   1. The partition for the filesystem the Ploopy's firmware exposes must be provided and exist.
+#   2. `ploopyco_madromys_rev1_001_holmes_software.uf2` (the compiled firmware) must exist in same directory as this script.
+#   3. The size of the partition must be exactly 134217216 bytes (just because I know that's what it's supposed to be).
+#   4. `INDEX.HTM` and `INFO_UF2.TXT` must exist in the mounted file system (again, I just know these are supposed to be
+#      there).
+
 #!/usr/bin/env bash
 set -euo pipefail
 
